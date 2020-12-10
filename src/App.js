646331b09e91044b2df2146a81a70cd94006bfe0
gergooo <39014407+gergooo@users.noreply.github.com>
2020-12-10 19:19:42 +0100

    some cleanup and initial skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <p>Movies and Vegetables Donate</p>
+        <p>Donate</p>
+      </div>
+
+      <div className="landing">
+        <h1>Movies and Vegetables</h1>
+        <p>A browser extension for movie fans.</p>
+      </div>
+
+      <div className="features">
+        <h2>Features</h2>
+        <p>Inserts stuff...</p>
+      </div>
+
+      <div className="download">
+        <h2>Download</h2>
+        <p>Download for Firefox!</p>
+        <p>Coming soon to Chrome.</p>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders extension name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const extensionNames = screen.getAllByText(/movies and vegetables/i);
+  extensionNames.forEach((name) => expect(name).toBeInTheDocument());
 });
