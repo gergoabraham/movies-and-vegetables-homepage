70930c46846bd824bde1c39c030e24743a4ff983
gergooo <39014407+gergooo@users.noreply.github.com>
2020-12-15 22:22:34 +0100

    header: the title is next to the logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,17 +4,17 @@ import Tomato from '../Icons/Tomato';
 import Clapperboard from '../Icons/Clapperboard';
 
 function Header({ className }) {
-  const style = { textAlign: 'center' };
-
   return (
-    <div className={'header ' + className} style={style}>
+    <div className={'header ' + className}>
       <div className="header__logo">
         <Tomato hover shadow className="header__logo__tomato" />
         <Clapperboard hover shadow className="header__logo__clapperboard" />
       </div>
-      <h1>Movies and Vegetables</h1>
-      <hr />
-      <p>A browser extension for movie fans.</p>
+      <div className="header__title">
+        <h1>Movies</h1>
+        <h2>and</h2>
+        <h1>Vegetables</h1>
+      </div>
     </div>
   );
 }
