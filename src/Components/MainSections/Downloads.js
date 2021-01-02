6638fc922790b57ac3345f38240a69cb1ca47007
gergooo <39014407+gergooo@users.noreply.github.com>
2021-01-02 18:18:49 +0100

    url to Firefox download

--- src/Components/MainSections/Downloads.js
+++ src/Components/MainSections/Downloads.js
@@ -4,7 +4,9 @@ function Downloads({ className }) {
   return (
     <div className={`download ${className || ''}`}>
       <h2>Downloads</h2>
-      <p>Download for Firefox!</p>
+      <a href="https://addons.mozilla.org/hu/firefox/addon/movies-and-vegetables/">
+        Download for Firefox!
+      </a>
       <p>Coming soon to Chrome.</p>
     </div>
   );
