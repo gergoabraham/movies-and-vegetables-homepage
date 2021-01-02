ef807d451e988d428d52be968dfdcba0fae41f39
gergooo <39014407+gergooo@users.noreply.github.com>
2021-01-02 16:57:41 +0100

    Features: ImagePart for showing features - 0. version

---
+++ src/Components/ImagePart/ImagePart.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import './ImagePart.css';
+
+function ImagePart({
+  children,
+  image = { name: '', width: 0, height: 0 },
+  part = { width: 0, height: 0, top: 0, left: 0 },
+}) {
+  const imagePath = './movies-and-vegetables-homepage/images/';
+  const [isHovered, setIsHovered] = useState(false);
+
+  const containerStyle = {
+    position: 'relative',
+    paddingBottom: '2rem',
+    paddingTop: `${part.height}px`,
+    width: `${part.width}px`,
+  };
+
+  const style = {
+    left: '0px',
+    top: '0px',
+    width: `${part.width}px`,
+    height: `${part.height}px`,
+    backgroundPosition: `${-part.left}px ${-part.top}px`,
+    backgroundImage: `url('${imagePath}${image.name}')`,
+  };
+
+  const hoveredStyle = {
+    zIndex: 10,
+    left: `${-part.left}px`,
+    top: `${-part.top}px`,
+    width: `${image.width}px`,
+    height: `${image.height}px`,
+    backgroundPosition: '0px 0px',
+    backgroundImage: `url('${imagePath}${image.name}')`,
+  };
+
+  return (
+    <div style={containerStyle}>
+      <div
+        style={{
+          position: 'absolute',
+          top: '0px',
+          left: '0px',
+          width: `${part.width}px`,
+          height: `${part.height}px`,
+          zIndex: '13',
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      ></div>
+
+      <CSSTransition
+        in={isHovered}
+        unmountOnExit
+        timeout={1100}
+        classNames="fade-in"
+      >
+        <div class="image-part-image" style={hoveredStyle}></div>
+      </CSSTransition>
+
+      <div class="image-part-image" style={style}></div>
+
+      {children}
+    </div>
+  );
+}
+
+export default ImagePart;
--- src/Components/MainSections/Features.js
+++ src/Components/MainSections/Features.js
@@ -1,10 +1,82 @@
+import FloatIn from '../FloatInAnimation/FloatIn';
+import ImagePart from '../ImagePart/ImagePart';
+
 import './Features.css';
 
 function Features({ className }) {
+  const imdbImage = { name: 'imdb-sample.png', width: 669, height: 801 };
+  const rottenImage = { name: 'rotten-sample.png', width: 524, height: 568 };
+
   return (
     <div className={'features ' + className}>
-      <h2>What does it do?</h2>
-      <p>Inserts stuff...</p>
+      <FloatIn>
+        <h2>What does it do?</h2>
+      </FloatIn>
+
+      <h3>Shows you the scores.</h3>
+      <div className="features__pages-container">
+        <div className="features__pages-container__page">
+          <h3>on IMDb</h3>
+
+          <ImagePart
+            image={imdbImage}
+            part={{ width: 288, height: 45, top: 49, left: 372 }}
+          >
+            Audience Score
+          </ImagePart>
+
+          <ImagePart
+            image={imdbImage}
+            part={{ width: 337, height: 37, top: 598, left: 13 }}
+          >
+            Tomatometer
+          </ImagePart>
+
+          <ImagePart
+            image={imdbImage}
+            part={{ width: 300, height: 58, top: 643, left: 13 }}
+          >
+            Critics Consensus
+          </ImagePart>
+        </div>
+
+        <div className="features__pages-container__page">
+          <h3>on RottenTomatoes</h3>
+
+          <ImagePart
+            image={rottenImage}
+            part={{ width: 164, height: 113, left: 268, top: 302 }}
+          >
+            IMDb Rating
+            <br />
+            (including Toplist Position)
+          </ImagePart>
+
+          <ImagePart
+            image={rottenImage}
+            part={{ width: 124, height: 113, left: 132, top: 302 }}
+          >
+            Metascore
+          </ImagePart>
+
+          <ImagePart
+            image={rottenImage}
+            part={{ width: 169, height: 72, left: 179, top: 417 }}
+          >
+            Summary
+          </ImagePart>
+        </div>
+      </div>
+
+      <h3>Leads you to the other site.</h3>
+      <div className="features__pages-container">
+        <div className="features__pages-container__page">
+          <h3>on IMDb</h3>
+        </div>
+        <div className="features__pages-container__page">
+          <h3>on RottenTomatoes</h3>
+        </div>
+      </div>
     </div>
   );
 }
