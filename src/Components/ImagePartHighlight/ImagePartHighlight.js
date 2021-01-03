55dabc971f4f68853510c66218824f50117fb247
gergooo <39014407+gergooo@users.noreply.github.com>
2021-01-03 18:42:44 +0100

    ImagePartHighlight: closest part to mouse is highlighted

--- src/Components/ImagePartHighlight/ImagePartHighlight.js
+++ src/Components/ImagePartHighlight/ImagePartHighlight.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import './ImagePartHighlight.css';
 
@@ -8,6 +8,9 @@ function ImagePartHighlight({
 }) {
   const [resizeFactor, setResizeFactor] = useState(1);
   const [highlightIndex, setHighlightIndex] = useState(0);
+  const [intervalCallbackId, setIntervalCallbackId] = useState(null);
+  const [isMouseIn, setIsMouseIn] = useState(false);
+  const ref = useRef();
 
   useEffect(() => {
     const windowResizeHandler = () => {
@@ -19,56 +22,113 @@ function ImagePartHighlight({
     window.addEventListener('resize', windowResizeHandler);
   }, [image.width]);
 
-  useEffect(() => {
-    const increaseHighlightIndex = () => {
-      setHighlightIndex((i) => (i < parts.length - 1 ? i + 1 : 0));
-    };
+  const proceedWithHighlightIndex = () => {
+    setHighlightIndex((i) => (i < parts.length - 1 ? i + 1 : 0));
+  };
 
-    setInterval(increaseHighlightIndex, 1500);
-  }, [parts.length]);
+  const onMouseEnterHandler = () => {
+    setIsMouseIn(true);
+    stopAutomaticHighlightStepper();
+  };
+
+  const onMouseLeaveHandler = () => {
+    setIsMouseIn(false);
+    startAutomaticHighlightStepper();
+  };
+
+  const startAutomaticHighlightStepper = () => {
+    if (!intervalCallbackId) {
+      const callbackId = setInterval(proceedWithHighlightIndex, 1500);
+      setIntervalCallbackId(callbackId);
+    }
+  };
+
+  const stopAutomaticHighlightStepper = () => {
+    if (intervalCallbackId) {
+      clearInterval(intervalCallbackId);
+      setIntervalCallbackId(null);
+    }
+  };
+
+  useEffect(() => {
+    startAutomaticHighlightStepper();
+  }, []);
+
+  const onMouseMovedHandler = (event) => {
+    const x2 = (event.clientX - ref.current.offsetLeft) / resizeFactor;
+    const y2 =
+      (event.clientY - ref.current.offsetTop + window.scrollY) / resizeFactor;
+
+    const smallestDistancesFromRectangles = parts.map((rect) => {
+      const x1 = Math.min(Math.max(x2, rect.left), rect.left + rect.width);
+      const y1 = Math.min(Math.max(y2, rect.top), rect.top + rect.height);
+
+      return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    });
+
+    const minDistanceIndex = smallestDistancesFromRectangles.reduce(
+      (minIndex, currentVal, currentIndex, array) =>
+        currentVal < array[minIndex] ? currentIndex : minIndex,
+      0
+    );
+
+    setHighlightIndex(minDistanceIndex);
+  };
 
   const part = parts[highlightIndex];
 
   return (
-    <div style={{ position: 'relative' }}>
+    <div
+      style={{ position: 'relative' }}
+      ref={ref}
+      onMouseEnter={onMouseEnterHandler}
+      onMouseLeave={onMouseLeaveHandler}
+      onMouseMove={onMouseMovedHandler}
+    >
       <div
+        className={
+          'image-part-highlight-background ' + (isMouseIn ? ' darkened' : '')
+        }
         style={{
           width: `${image.width * resizeFactor}px`,
           height: `${image.height * resizeFactor}px`,
-          borderRadius: '1rem',
 
           backgroundImage: `url(${image.url})`,
-          backgroundSize: 'contain',
         }}
       ></div>
 
-      {
-        <div
-          title={part.name}
-          style={{
-            position: 'absolute',
+      <div
+        className="image-part-highlight-highlight"
+        style={{
+          top: `${part.top * resizeFactor}px`,
+          left: `${part.left * resizeFactor}px`,
+          width: `${part.width * resizeFactor}px`,
+          height: `${part.height * resizeFactor}px`,
 
-            top: `${part.top * resizeFactor}px`,
+          filter: isMouseIn ? 'none' : 'brightness(1.5)',
+
+          backgroundImage: `url(${image.url})`,
+          backgroundSize:
+            `${image.width * resizeFactor}px ` +
+            `${image.height * resizeFactor}px`,
+          backgroundPosition:
+            `${-part.left * resizeFactor}px ` + `${-part.top * resizeFactor}px`,
+        }}
+      ></div>
+
+      {isMouseIn && (
+        <div
+          className="image-part-highlight-legend"
+          style={{
+            top: `${(part.top + part.height) * resizeFactor}px`,
             left: `${part.left * resizeFactor}px`,
             width: `${part.width * resizeFactor}px`,
             height: `${part.height * resizeFactor}px`,
-
-            borderRadius: '0.5rem',
-
-            backgroundImage: `url(${image.url})`,
-            backgroundSize:
-              `${image.width * resizeFactor}px ` +
-              `${image.height * resizeFactor}px`,
-            backgroundPosition:
-              `${-part.left * resizeFactor}px ` +
-              `${-part.top * resizeFactor}px`,
-
-            filter: 'brightness(1.6)',
-            transitionDuration: '500ms',
-            transitionTimingFunction: 'ease',
           }}
-        ></div>
-      }
+        >
+          <div className="image-part-highlight-legend-text">{part.name}</div>
+        </div>
+      )}
     </div>
   );
 }
